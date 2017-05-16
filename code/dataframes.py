4094d858928c1eeb8eff2b03350e29a61548f6fc
Ky Kiefer <kkiefer@umich.edu>
2017-05-16 10:13:56 -0600

    combine train and test dataframes

---
+++ code/dataframes.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+df_train = pd.read_csv('/Users/ky/Desktop/depression-detect/raw_data/labels/train_split_Depression_AVEC2017.csv')
+
+df_test = pd.read_csv('/Users/ky/Desktop/depression-detect/raw_data/labels/dev_split_Depression_AVEC2017.csv')
+
+df_participants = pd.concat([df_train, df_test], axis=0)
+
+# df_holdout = pd.read_csv('/Users/ky/Desktop/depression-detect/raw_data/labels/dev_split_Depression_AVEC2017.csv')
