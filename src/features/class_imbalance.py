ff13c0a09d20edfe94b44f47444a7910ee55c32c
Ky Kiefer <kkiefer@umich.edu>
2017-05-18 18:40:54 -0600

    established crop size

--- src/features/class_imbalance.py
+++ src/features/class_imbalance.py
@@ -2,13 +2,22 @@ import os
 from dataframes import df_dev
 from PIL import Image
 
-"""
-There exists a large data imbalance between positive and negative samples, which incurs a large bias in classification. The number of non-depressed subjects is about four times bigger than that of depressed ones. If these samples for learning, the model will have a strong bias to the non-depressed class. Moreover, regarding the length of each sample, a much longer signal of an individual may emphasize some characteristics that are person specific.
-
-To solve the problem, I perform random cropping on each of the particpant's spectrograms of a specified width (time) and contstant height (frquency -- the whole spectrum), to ensure the CNN has an equal proportion for every subject and each class.
-"""
 
 def get_class_imbalance(rootdir):
+    """
+    Using the width (represeting time) time of each particpant's no_silence.png spectrogram, return a dictionary with keys of particpants and values of png width. This can be used to quantify how much audio/spectrogram is available for each class (depressed/non-depressed) and each speaker (particpant)
+
+    Parameters
+    ----------
+    rootdir : filepath
+        directory containing participant's folders (which contains the no_silence.png)
+    Returns
+    -------
+    depressed_dict : dictionary
+        dictionary of depressed individuals with keys of particiapnt id and values of no_silence spectrogram width
+    normal_dict : dictionary
+        dictionary of non-depressed individuals with keys of particiapnt id and values of no_silence spectrogram width
+    """
     depressed_dict = dict()
     normal_dict = dict()
     for subdir, dirs, files in os.walk(rootdir):
@@ -34,9 +43,15 @@ def get_weight(png_file):
     return width
 
 def get_depression_label(partic_id):
+    """
+    Returns participant's PHQ8 Binary label. 1 representing depression; 0 representing no depression.
+    """
     return df_dev.loc[df_dev['Participant_ID'] == partic_id]['PHQ8_Binary'].item()
 
 def in_dev_split(partic_id):
+    """
+    Returns True if the participant is in the AVEC developemnt split (aka particpant's we have depression labels for)
+    """
     return partic_id in set(df_dev['Participant_ID'].values)
 
 if __name__ == '__main__':
