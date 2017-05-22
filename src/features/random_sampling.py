d8233f48c2ec7248f150f6999e62623dfc1cf544
Ky Kiefer <kkiefer@umich.edu>
2017-05-21 21:46:12 -0600

    implemented np.split for splitting array in lieu of for loop

--- src/features/random_sampling.py
+++ src/features/random_sampling.py
@@ -1,5 +1,8 @@
 import numpy as np
+import random
 from spectrogram_dicts import build_class_dictionaries
+np.random.seed(15)  # for reproducibility
+
 
 """
 There exists a large data imbalance between positive and negative samples, which incurs a large bias in classification. The number of non-depressed subjects is about four times bigger than that of depressed ones. If these samples for learning, the model will have a strong bias to the non-depressed class. Moreover, regarding the length of each sample, a much longer signal of an individual may emphasize some characteristics that are person specific.
@@ -14,23 +17,24 @@ Some success has been found using random crops of 3-5 seconds [1]. I'll start wi
 """
 
 
-def determine_num_crops(depressed_dict, crop_width=125):
+def determine_num_crops(depressed_dict, normal_dict, crop_width=125):
     """
-    Finds the shortest clip in the depressed class, which, according to our random sampling strategy, will limit the number of samples we take from each clip to make sure our classes are balanced.
+    Finds the shortest clip in the entire dataset which, according to our random sampling strategy, will limit the number of samples we take from each clip to make sure our classes are balanced.
 
     Parameters
     ----------
     depressed_dict : dictionary
         a dictionary of depressed particpants with the participant id as the key and the segmented and concatentated matrix representation of their spectrograms as the values.
     crop_width : integer
-        the desired pixel width of the crop samples (125 pixes = 4 seconds of audio)
+        the desired pixel width of the crop samples (125 pixels = 4 seconds of audio)
 
     Returns
     -------
     num_samples_from_clips : int
         the maximum number of samples that should be sampled from each clip to ensure balanced classes can be built
     """
-    shortest_clip = min(depressed_dict.items(), key=lambda x: x[1].shape[1])  # returns dictionary entry
+    merged_dict = dict(normal_dict, **depressed_dict)
+    shortest_clip = min(merged_dict.items(), key=lambda x: x[1].shape[1])
     shortest_pixel_width = shortest_clip[1].shape[1]
     num_samples_from_clips = shortest_pixel_width / crop_width
     return num_samples_from_clips
@@ -57,46 +61,34 @@ def get_samples_from_class(segmented_audio_dict, n_samples, crop_width):
     class_samples_dict = dict()
     for partic_id, clip_mat in segmented_audio_dict.iteritems():
             # print(partic_id)
-            samples = random_non_overlapping_samples(clip_mat, n_samples, crop_width)
+            samples = get_random_samples(clip_mat, n_samples, crop_width)
             class_samples_dict[partic_id] = samples
     return class_samples_dict
 
 
-def random_non_overlapping_samples(matrix, n_samples, crop_width):
+def get_random_samples(matrix, n_samples, crop_width):
     """
     Get N pseudo-random samples with width of crop_width from the numpy matrix representing the partiipants audio spectrogram.
     """
-    width = matrix.shape[1]
-    freedom = width - (n_samples * crop_width)  # available width - width to be sampled
-    # print(width)
-    # print('{} samples with {} crop width'.format(n_samples, crop_width))
-    # print('{} pixels of freedom'.format(freedom))
+    clipped_mat = matrix[:, (matrix.shape[1] % crop_width):]  # turn into width divisible by crop_width
+    n_splits = clipped_mat.shape[1] / crop_width
+    cropped_sample_ls = np.split(clipped_mat, n_splits, axis=1)
 
-    partic_samples = []
-    start_col = 0
-    for sample in range(n_samples):
-        offset = np.random.randint(0, freedom)  # randomness gets eaten up pretty quickly -- come back to this...maybe okay
-        freedom -= offset  # remaining freedom
-        start_col += offset
-        end_col = start_col + crop_width
-        crop = matrix[:, start_col:end_col]  # all frequency bins; specified time slice
-        partic_samples.append(crop)
-        start_col += crop_width
-    return partic_samples
+    # get random samples
+    samples = random.sample(cropped_sample_ls, n_samples)
+    return samples
 
 
-def create_sample_dicts():
+def create_sample_dicts(crop_width):
     """
     Utilizes the above function to return two dictionaries, depressed and normal. Each dictionary has only participants in the specific class, with participant ids as key, a values of a list of the cropped samples from the spectrogram matrices. The lists are n_samples long and the entries within the list have dimension
     """
-    np.random.seed(15)  # for reproducibility
-    crop_width = 125  # 125 pixels = 4 seconds of audio
     # build dictionaries of participants and segmented audio matrix
     depressed_dict, normal_dict = build_class_dictionaries('/Users/ky/Desktop/depression-detect/data/interim')
-    n_samples = determine_num_crops(depressed_dict, crop_width=crop_width)
-    # get n_sample pseudo-random samples from each depressed participant
+    n_samples = determine_num_crops(depressed_dict, normal_dict, crop_width=crop_width)
+    # get n_sample random samples from each depressed participant
     depressed_samples = get_samples_from_class(depressed_dict, n_samples, crop_width)
-    # get n_sample pseudo-random samples from each non-depressed participant
+    # get n_sample random samples from each non-depressed participant
     normal_samples = get_samples_from_class(normal_dict, n_samples, crop_width)
     return depressed_samples, normal_samples
 
@@ -109,4 +101,5 @@ def test_train_split(test_size=0.25):
 
 if __name__ == '__main__':
     # test_train_split(test_size=0.25)
-    depressed_dict, normal_dict = build_class_dictionaries('/Users/ky/Desktop/depression-detect/data/interim')
+    # depressed_dict, normal_dict = build_class_dictionaries('/Users/ky/Desktop/depression-detect/data/interim')
+    depressed_samples, normal_samples = create_sample_dicts(crop_width=125)  # 125 pixels = 4 seconds of audio
