7872064bede96c90c9749f795257beb424678473
Ky Kiefer <kkiefer@umich.edu>
2017-05-30 16:35:49 -0600

    rendering blank pages

--- web_app/app.py
+++ web_app/app.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-# home page
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/donate')
+def donate():
+    return render_template('donate.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True, threaded=True)
