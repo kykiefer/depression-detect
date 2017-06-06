89b30fba09bd48e482bd0c4956e7bedff9c0e889
Ky Kiefer <kkiefer@umich.edu>
2017-06-05 19:11:12 -0600

    cnn modeled off envirnomental sound classification

--- src/features/cnn.py
+++ src/features/cnn.py
@@ -11,6 +11,7 @@ from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras.utils import np_utils
 from keras import backend as K
+from keras.optimizers import SGD
 K.set_image_dim_ordering('th')
 access_key = os.environ['AWS_ACCESS_KEY_ID']
 access_secret_key = os.environ['AWS_SECRET_ACCESS_KEY']
@@ -34,16 +35,16 @@ def retrieve_from_bucket(file):
     X = np.load(file)
     return X
 
-def preprocess(X_train, X_test):
-    """
-    Convert from float64 to float32 for speed.
-    """
-    X_train = X_train.astype('float32')
-    X_test = X_test.astype('float32')
-    # normalize
-    X_train = np.array([X / X.max() for X in X_train])
-    X_test = np.array([X / X.max() for X in X_test])
-    return X_train, X_test
+# def preprocess(X_train, X_test):
+#     """
+#     Convert from float64 to float32 for speed.
+#     """
+#     X_train = X_train.astype('float32')
+#     X_test = X_test.astype('float32')
+#     # normalize
+#     X_train = np.array([X / X.max() for X in X_train])
+#     X_test = np.array([X / X.max() for X in X_test])
+#     return X_train, X_test
 
 
 def train_test(X, y, nb_classes, test_size=0.2):
@@ -70,10 +71,9 @@ def train_test(X, y, nb_classes, test_size=0.2):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=15, stratify=y)
 
     print('X_train shape:', X_train.shape)
-    print(X_train.shape[0], 'train samples')
-    print(X_test.shape[0], 'test samples')
+    print('Train on {} samples, validate on {}'.format(X_train.shape[0], X_test.shape[0]))
 
-    X_train, X_test = preprocess(X_train, X_test)
+    # X_train, X_test = preprocess(X_train, X_test)
 
     # Convert class vectors to binary class matrices
     Y_train = np_utils.to_categorical(y_train, nb_classes)
@@ -85,8 +85,8 @@ def train_test(X, y, nb_classes, test_size=0.2):
 def keras_img_prep(X_train, X_test, img_dep, img_rows, img_cols):
     """
     Reshape feature matrices for Keras' expexcted input dimensions.
-    For 'tf' (TensorFlow) dim_order, the model expects dimensions:
-    (# images, # rows, # cols, # channels).
+    For 'th' (Theano) dim_order, the model expects dimensions:
+    (# channels, # images, # rows, # cols).
     """
     if K.image_dim_ordering() == 'th':
         X_train = X_train.reshape(X_train.shape[0], 1, img_rows, img_cols)
@@ -106,20 +106,22 @@ def cnn(X_train, y_train, X_test, y_test, kernel_size, pool_size, batch_size, nb
     """
     model = Sequential()
 
-    model.add(Conv2D(32, (9, 9), padding='valid', input_shape=input_shape, activation='relu'))
-    model.add(Conv2D(32, (9, 9), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(7, 7)))
-    model.add(Dropout(0.25))
+    model.add(Conv2D(32, (57, 6), padding='valid', strides=1, input_shape=input_shape, activation='relu'))
+    model.add(MaxPooling2D(pool_size=(4,3), strides=(1,3)))
+    model.add(Conv2D(32, (1, 3), padding='valid', strides=1, input_shape=input_shape, activation='relu'))
+    model.add(MaxPooling2D(pool_size=(1,3), strides=(1,3)))
 
     model.add(Flatten())
+    model.add(Dense(128, activation='softmax'))
     model.add(Dense(128, activation='relu'))
-    model.add(Dropout(0.5))
     model.add(Dense(nb_classes))
     model.add(Activation('softmax'))
 
+
+    sgd = SGD(lr=0.001, decay=1e-6, momentum=1.0)
     # Configure the model, using AdaDelta Gradient Descent optimization
     model.compile(loss='categorical_crossentropy',
-                  optimizer='adadelta',
+                  optimizer=sgd,
                   metrics=['accuracy'])
 
     model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs,
@@ -153,35 +155,52 @@ if __name__ == '__main__':
     X = np.load('samples.npz')['arr_0']
     y = np.load('labels.npz')['arr_0']
 
-    # troubleshooting - subsample 20 from each class
-    # X = np.concatenate((X[:20], X[-20:]))
-    # y = np.concatenate((y[:20], y[-20:]))
+    # # troubleshooting - subsample 10 from each class
+    # X = np.concatenate((X[:10], X[-10:]))
+    # y = np.concatenate((y[:10], y[-10:]))
+
+    # more testing
+    samples = []
+    depressed = np.load('/Users/ky/Desktop/depression-detect/data/processed/D319.npz')
+    for key in depressed.keys():
+        samples.append(depressed[key])
+    normal = np.load('/Users/ky/Desktop/depression-detect/data/processed/N303.npz')
+    for key in normal.keys():
+        samples.append(normal[key])
+    X = np.array(samples)
+    y = np.concatenate((np.ones(40), np.zeros(40)))
+
+    print('X shape', X.shape)
 
     # # troubleshooting - downsample images -- mean doesn't work for decibels
     # X = np.array([block_reduce(x, block_size=(4, 1), func=np.mean) for x in X])
 
     # bottom of the frequnecy spectrum
-    # X = np.array([x[-125:, :] for x in X])
+    # X = np.array([x[125:, :] for x in X])
 
     # CNN parameters
-    batch_size = 76
+    batch_size = 16
     nb_classes = 2
-    epochs = 2
+    epochs = 30
     kernel_size = (9, 9)  # (width, height)
     pool_size = (7, 7)
     nb_filters = 32
 
-    # train/test split for cross validation
+    # train/test split
     test_size = 0.2
-    X_train, X_test, y_train, y_test = train_test(X, y, 2, test_size=test_size)
+    X_train, X_test, y_train, y_test = train_test(X, y, nb_classes=2, test_size=test_size)
 
     # specify image dimensions - 513x125x1 for spectrogram with crop size of 125 pixels
     img_rows, img_cols, img_depth = X_train.shape[1], X_train.shape[2], 1
 
     # prep image input for Keras
-    # used TensorFlow dim_ordering (tf), (# images, # rows, # cols, # chans)
-    # used Theano backend, tf dim_ordering
+    # used Theano dim_ordering (th), (# images, # chans, # rows, # cols)
     X_train, X_test, input_shape = keras_img_prep(X_train, X_test, img_depth, img_rows, img_cols)
+
+    print('X_train shape', X_train.shape)
+    print('input shape', input_shape)
+
+    print(K.image_data_format())
 
     # run CNN
     model = cnn(X_train, y_train, X_test, y_test, kernel_size, pool_size, batch_size, nb_classes, epochs, input_shape)
