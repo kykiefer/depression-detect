4b7aa5a20a196d83d1cc851115f3f19d817dedc8
Ky Kiefer <kkiefer@umich.edu>
2017-06-10 19:05:08 -0600

    starting to refactor...needs work

--- src/features/random_sampling.py
+++ src/features/random_sampling.py
@@ -69,7 +69,7 @@ def build_class_sample_dict(segmented_audio_dict, n_samples, crop_width):
         a dictionary of a class of participants with keys of participant ids \
         and values of the segmented audio matrix spectrogram representation
     n_samples : integer
-        number of pseudo-random non-overlapping samples to extract from each \
+        number of random non-overlapping samples to extract from each \
         segmented audio matrix spectrogram
     crop_width : integer
         the desired pixel width of the crop samples \
@@ -85,7 +85,6 @@ def build_class_sample_dict(segmented_audio_dict, n_samples, crop_width):
     """
     class_samples_dict = dict()
     for partic_id, clip_mat in segmented_audio_dict.iteritems():
-            # print(partic_id)
             samples = get_random_samples(clip_mat, n_samples, crop_width)
             class_samples_dict[partic_id] = samples
     return class_samples_dict
@@ -94,9 +93,9 @@ def build_class_sample_dict(segmented_audio_dict, n_samples, crop_width):
 def get_random_samples(matrix, n_samples, crop_width):
     """
     Get N random samples with width of crop_width from the numpy matrix \
-    representing the participants audio spectrogram.
+    representing the participant's audio spectrogram.
     """
-    # turn into width divisible by crop_width
+    # crop full spectrogram into segments of width = crop_width
     clipped_mat = matrix[:, (matrix.shape[1] % crop_width):]
     n_splits = clipped_mat.shape[1] / crop_width
     cropped_sample_ls = np.split(clipped_mat, n_splits, axis=1)
@@ -111,8 +110,9 @@ def create_sample_dicts(crop_width):
     Utilizes the above function to return two dictionaries, depressed \
     and normal. Each dictionary has only participants in the specific class, \
     with participant ids as key, a values of a list of the cropped samples \
-    from the spectrogram matrices. The lists are n_samples long and the \
-    entries within the list have dimension.
+    from the spectrogram matrices. The lists are vary in length depending \
+    on the length of the interview clip. The entries within the list are \
+    numpy arrays with dimennsion (513, 125).
     """
     # build dictionaries of participants and segmented audio matrix
     depressed_dict, normal_dict = build_class_dictionaries('../../data/interim')
@@ -124,6 +124,8 @@ def create_sample_dicts(crop_width):
     # get n_sample random samples from each non-depressed participant
     normal_samples = build_class_sample_dict(normal_dict, n_samples,
                                              crop_width)
+    # iterate through samples dictionaries and save a npz file
+    # with the radomly sleected n_samples for each participant.
     # save depressed arrays to .npz
     for key, _ in depressed_samples.iteritems():
         path = '../../data/processed/'
@@ -136,11 +138,29 @@ def create_sample_dicts(crop_width):
         filename = '/N{}.npz'.format(key)
         outfile = path + filename
         np.savez(outfile, *normal_samples[key])
-    return n_samples
 
 
-def build_array_of_random_samples(npz_file_dir):
-    # print files in directory
+def rand_samp_train_test_split(npz_file_dir):
+    """
+    Given the cropped segments from each class and particpant, this fucntion \
+    determines how many samples we can draw from each particpant and how many \
+    participants we can draw from each class.
+
+    Parameters
+    ----------
+    npz_file_dir : directory
+        directory contain the
+    crop_width : integer
+        the desired pixel width of the crop samples \
+        (125 pixels = 4 seconds of audio)
+
+    Returns
+    -------
+    num_samples_from_clips : int
+        the maximum number of samples that should be sampled from each clip \
+        to ensure balanced classes can be built.
+    """
+    # files in directory
     npz_files = os.listdir(npz_file_dir)
 
     dep_samps = [f for f in npz_files if f.startswith('D')]
@@ -157,18 +177,18 @@ def build_array_of_random_samples(npz_file_dir):
     # randomly select n_samples_per_person (40 in the case of a crop width
     # of 125) from each of the participant lists
 
-    # refactor this code!
+    # REFACTOR this code!
     test_size = 0.2
     num_test_samples = int(len(dep_select_samps) * test_size)
 
     train_samples = []
     for sample in dep_select_samps[:-num_test_samples]:
-        npz_file = npz_file_dir +  '/' + sample
+        npz_file = npz_file_dir + '/' + sample
         with np.load(npz_file) as data:
             for key in data.keys():
                 train_samples.append(data[key])
     for sample in norm_select_samps[:-num_test_samples]:
-        npz_file = npz_file_dir +  '/' + sample
+        npz_file = npz_file_dir + '/' + sample
         with np.load(npz_file) as data:
             for key in data.keys():
                 train_samples.append(data[key])
@@ -177,50 +197,51 @@ def build_array_of_random_samples(npz_file_dir):
 
     test_samples = []
     for sample in dep_select_samps[-num_test_samples:]:
-        npz_file = npz_file_dir +  '/' + sample
+        npz_file = npz_file_dir + '/' + sample
         with np.load(npz_file) as data:
             for key in data.keys():
                 test_samples.append(data[key])
     for sample in norm_select_samps[-num_test_samples:]:
-        npz_file = npz_file_dir +  '/' + sample
+        npz_file = npz_file_dir + '/' + sample
         with np.load(npz_file) as data:
             for key in data.keys():
                 test_samples.append(data[key])
     test_labels = np.concatenate((np.ones(len(test_samples)/2),
                                   np.zeros(len(test_samples)/2)))
 
-    return np.array(train_samples), train_labels, np.array(test_samples),
-    test_labels
+    return np.array(train_samples), train_labels, np.array(test_samples), \
+        test_labels
 
 
 def save_to_bucket(file, obj_name):
     """
-    Saves local file to S3 bucket for redundancy and reproducibility \
-    by others.
+    Saves local file to S3 bucket for redundancy and reproducibility.
     """
     conn = boto.connect_s3(access_key, access_secret_key)
-
     bucket = conn.get_bucket('depression-detect')
-
     file_object = bucket.new_key(obj_name)
     file_object.set_contents_from_filename(file)
 
 
 if __name__ == '__main__':
-    # build particpants npz files
-    # create_sample_dicts(crop_width=125)
-    train_samples, train_labels, test_samples, test_labels \
-        = build_array_of_random_samples('../../data/processed')
+    # build participant's cropped npz files
+    # this is of the whole no_silence particpant's no_silence interview,
+    # but each array in the npz files was width of crop_width
+    create_sample_dicts(crop_width=125)
+
+    # random sample from particpants npz files to ensure class balance
+    train_samples, train_labels, test_samples, \
+        test_labels = rand_samp_train_test_split('../../data/processed')
 
     # save as npz locally
-    print("Saving locally...")
+    print("Saving npz file locally...")
     np.savez('../../data/processed/train_samples.npz', train_samples)
     np.savez('../../data/processed/train_labels.npz', train_labels)
     np.savez('../../data/processed/test_samples.npz', test_samples)
     np.savez('../../data/processed/test_labels.npz', test_labels)
 
     # upload npz files to S3 bucket for accessibility on AWS
-    print("Uploading to S3...")
+    print("Uploading npz to S3...")
     save_to_bucket('../../data/processed/train_samples.npz', 'train_samples.npz')
     save_to_bucket('../../data/processed/train_labels.npz', 'train_labels.npz')
     save_to_bucket('../../data/processed/test_samples.npz', 'test_samples.npz')
